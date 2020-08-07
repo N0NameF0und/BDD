0244d4ccc59fb9cd6f433a029ff62feec5dd61b2
N0NameF0und@github.com <kam.zapata@yavirac.edu.ec>
2020-08-07 12:38:35 -0500

    pagina inicio

---
+++ SpecFlowJuegoMinas/Features/Minas.feature
@@ -0,0 +1,12 @@
+﻿# language: es
+Característica: JuegoMinas
+	Para poder entrenerme
+	como un jugador
+	quiero un juego de minas 
+	
+
+@mytag
+Escenario: Ingreso al juego
+	Dado que me registro en el sistema
+	Cuando ingreso mi nombre
+	Entonces el juego inicializara 
---
+++ SpecFlowJuegoMinas/Steps/JuegoMinasSteps.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowJuegoMinas.Steps
+{
+    [Binding]
+    public class JuegoMinasSteps
+    {
+        IWebDriver webDriver = new FirefoxDriver();
+
+        [Given(@"que me registro en el sistema")]
+        public void DadoQueMeRegistroEnElSistema()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"ingreso mi nombre")]
+        public void CuandoIngresoMiNombre()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"el juego inicializara")]
+        public void EntoncesElJuegoInicializara()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
