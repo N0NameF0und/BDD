1f89db0db4889ce60a4e611fa908a052e3258c38
N0NameF0und@github.com <kam.zapata@yavirac.edu.ec>
2020-08-11 23:33:15 -0500

    update

--- Juego/Startup.cs
+++ Juego/Startup.cs
@@ -51,8 +51,14 @@ namespace Juego
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+             /* endpoints.MapRazorPages();*/
+                endpoints.MapControllerRoute(
+                name: "default",
+                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
+
+
         }
     }
 }
