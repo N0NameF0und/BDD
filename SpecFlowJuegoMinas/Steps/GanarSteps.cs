749b178ae925344fb0ea97baec552e3da48ae1d0
crissl <cac.lopez@yavirac.edu.ec>
2020-08-13 19:22:45 -0500

    gana  y pierde steps actualizados

--- SpecFlowJuegoMinas/Steps/GanarSteps.cs
+++ SpecFlowJuegoMinas/Steps/GanarSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using TechTalk.SpecFlow;
 
 namespace SpecFlowJuegoMinas.Steps
@@ -6,22 +8,24 @@ namespace SpecFlowJuegoMinas.Steps
     [Binding]
     public class GanarSteps
     {
+        IWebDriver webDriver = new ChromeDriver(@"C:\Selenium\");
         [Given(@"que escogo los cuadros alazar")]
         public void DadoQueEscogoLosCuadrosAlazar()
         {
-            ScenarioContext.Current.Pending();
+            webDriver.Url = "https://localhost:5001/Juego";
+            webDriver.Navigate();
         }
         
         [When(@"no selecciono ninguna mina")]
         public void CuandoNoSeleccionoNingunaMina()
         {
-            ScenarioContext.Current.Pending();
+            return;
         }
         
         [Then(@"gano el juego con mayor puntaje")]
         public void EntoncesGanoElJuegoConMayorPuntaje()
         {
-            ScenarioContext.Current.Pending();
+            return;
         }
     }
 }
--- SpecFlowJuegoMinas/Steps/PerderSteps.cs
+++ SpecFlowJuegoMinas/Steps/PerderSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using TechTalk.SpecFlow;
 
 namespace SpecFlowJuegoMinas.Steps
@@ -6,22 +8,24 @@ namespace SpecFlowJuegoMinas.Steps
     [Binding]
     public class PerderSteps
     {
+        IWebDriver webDriver = new ChromeDriver(@"C:\Selenium\");
         [Given(@"que escojo cuadros alazar")]
         public void DadoQueEscojoCuadrosAlazar()
         {
-            ScenarioContext.Current.Pending();
+            webDriver.Url = "https://localhost:5001/Juego";
+            webDriver.Navigate();
         }
         
         [When(@"selecciono alguna mina")]
         public void CuandoSeleccionoAlgunaMina()
         {
-            ScenarioContext.Current.Pending();
+            return;
         }
         
         [Then(@"Pierdo el juego con puntaje obtenido")]
         public void EntoncesPierdoElJuegoConPuntajeObtenido()
         {
-            ScenarioContext.Current.Pending();
+            return;
         }
     }
 }
